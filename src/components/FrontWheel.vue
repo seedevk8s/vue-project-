64c345960e1333f4556bdf307bcac19dbe6a44c4
seedevk8s <seedevk8s@gmail.com>
2025-04-14 13:48:13 +0900

    first commit

---
+++ src/components/FrontWheel.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Tire from "@/components/Tire.vue";
+</script>
+
+<template>
+  <div class="frontWheel">
+    <span>FRONT WHEEL</span>
+    <Tire/>
+  </div>
+</template>
+
+<style scoped>
+.frontWheel {
+  border: 1px solid black;
+  border-radius: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 150px;
+  height: 150px;
+}
+</style>
